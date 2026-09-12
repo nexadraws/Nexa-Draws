2f807fd74be5ebed38fe770e23b3bc303bfd838a
nexadraws <thomaswalters1996@hotmail.com>
2026-09-12 15:38:55 +0100

    Change PAYMENT_PROVIDER from DNA Payments to Nochex

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const ADMIN_UID =
   '2b4b64c6-b96f-4b85-bce3-be28c141311e';
 
 const PAYMENT_MODE = 'disabled';
-const PAYMENT_PROVIDER = 'DNA Payments';
+const PAYMENT_PROVIDER = 'Nochex';
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
